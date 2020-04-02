e19d416492be9e4c89a182f6de8db3f40ae11d4e
Yuelin-bit <yuelinapp@163.com>
2020-04-02 18:10:21 -0400

    #2 BitCoin Model

--- EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/model/Bitcoin.java
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/model/Bitcoin.java
@@ -2,11 +2,22 @@ package ca.mcgill.ecse321.eventregistration.model;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Bitcoin {
-	private int amount;
+	private String bitCoinUserId;
+	@Id
+	public String getBitCoinUserId() {
+		return bitCoinUserId;
+	}
 
+	public void setBitCoinUserId(String bitCoinUserId) {
+		this.bitCoinUserId = bitCoinUserId;
+	}
+	
+	private int amount;
+	
 	public int getAmount() {
 		return amount;
 	}
@@ -14,15 +25,5 @@ public class Bitcoin {
 	public void setAmount(int amount) {
 		this.amount = amount;
 	}
-	
-	@Id
-	private int id;
 
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
 }
