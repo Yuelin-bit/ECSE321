47d81926879af4703e10704d15ac26de93c150dd
Yuelin-bit <yuelinapp@163.com>
2020-04-01 23:01:55 -0400

    #2 #3 Promoter

--- EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/model/Promoter.java
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/model/Promoter.java
@@ -1,8 +1,21 @@
 package ca.mcgill.ecse321.eventregistration.model;
 
+import java.util.Set;
+
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
 
 @Entity
-public class Promoter {
-
+public class Promoter extends Person {
+	private Set<Event> events;
+	
+	@OneToMany(mappedBy="managingEvents" )
+	public Set<Event> getEvents(){
+	     return this.events;
+	}
+	
+	public void setEvents(Set<Event> events) {
+		  this.events = events;
+	}
 }
