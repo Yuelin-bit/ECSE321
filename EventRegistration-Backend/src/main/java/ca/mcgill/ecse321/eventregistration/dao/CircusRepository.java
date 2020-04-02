f4cd02942501e8635c5d66c1ceaa63fc5f3e08ec
Yuelin-bit <yuelinapp@163.com>
2020-04-02 18:16:39 -0400

    #3 Circus and Bitcoin DAO

---
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/dao/CircusRepository.java
@@ -0,0 +1,9 @@
+package ca.mcgill.ecse321.eventregistration.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import ca.mcgill.ecse321.eventregistration.model.Circus;
+
+public interface CircusRepository extends CrudRepository<Circus, String> {
+	Circus findCircusByName(String name);
+}
